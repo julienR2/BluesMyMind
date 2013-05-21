dacedf1b103acb6897f6c739b926e5f91957b078
louise <louve.grandjonc@gmail.com>
2013-05-21 21:25:27 +0200

    save as midi

--- source/generate_composition.py
+++ source/generate_composition.py
@@ -20,7 +20,12 @@ def generate_composition(pattern, progression_type, nb_bars, mode='none', key="C
     left_hand = generate_pattern(progression_list, key, pattern, nb_bars)
     newComposition.add_track(left_hand)
    
-    MidiFileOut.write_Composition("myComposition.mid", newComposition, rythm, False)
+    return newComposition
+
+def save_as_midi(name, composition, rythm):
+    MidiFileOut.write_Composition(name+".mid", composition, rythm, False)
+
+#TODO save_as_pdf
 
 
 #example : generate_composition(((1, 8, 1, "none", '=', 2), (5, 8, 1, "none", '+', 2), (1, 8, 1.5, "none", '=', 2), (5, 8, 1.5, "none", '+', 2), (2, 8, 2, "diese", '=', 2), (3, 8, 2.5, "none", '+', 2), (1, 8, 3, "none", '=', 2), (5, 8, 3, "none", '+', 2), (1, 8, 3.5, "none", '=', 2), (5, 8, 3.5, "none", '+', 2), (2, 8, 4, "diese", '=', 2), (3, 8, 4.5, "none", '+', 2)), 12, 1, 'none', "D", 60)    
